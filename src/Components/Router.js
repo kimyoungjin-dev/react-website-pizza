de7c77ea302a25f330ababefa35539bc9270c948
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-06 18:13:44 +0900

    Import screens other than the home screen and pass the isOpen property to home.js.

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,17 +4,25 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-import Home from "Screens/Home";
 import Nav from "Components/Nav";
+import Home from "Screens/Home/Home";
+import Desserts from "Screens/Desserts";
+import Fullmenu from "Screens/Fullmenu";
+import Ordernow from "Screens/Ordernow";
+import SliderBar from "Components/SliderBar";
 
-export default () => {
+export default ({ isOpen, setIsOpen }) => {
   return (
     <Router>
-      <Nav />
+      <Nav setIsOpen={setIsOpen} />
+      <SliderBar isOpen={isOpen} setIsOpen={setIsOpen} />
       <Switch>
         <Route exact path="/" component={Home}>
-          <Home />
+          <Home isOpen={isOpen} setIsOpen={setIsOpen} />
         </Route>
+        <Route path="/desserts" component={Desserts} />
+        <Route path="/fullmenu" component={Fullmenu} />
+        <Route path="/ordernow" component={Ordernow} />
         <Redirect from="*" to="/" />
       </Switch>
     </Router>
