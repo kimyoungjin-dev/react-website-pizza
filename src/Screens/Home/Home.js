9512c27d36754f1277e91147d4b34106acb06407
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-06 19:10:22 +0900

    Delete unused imports

--- src/Screens/Home/Home.js
+++ src/Screens/Home/Home.js
@@ -1,11 +1,10 @@
 import React from "react";
 import HomeCenter from "./HomeCenter";
-import SliderBar from "Components/SliderBar";
 import Products from "./Products";
 import Feature from "Screens/Home/Feature";
 import HomeBread from "./HomeBread";
 
-const Home = ({ isOpen, setIsOpen }) => {
+const Home = () => {
   return (
     <>
       <HomeCenter />
