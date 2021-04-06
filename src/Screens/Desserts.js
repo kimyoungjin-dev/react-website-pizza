97289bb53fe84733f5c50c94107bf37235fba044
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-06 18:14:21 +0900

    Make a screen

---
+++ src/Screens/Desserts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const Desserts = () => {
+  return <Container></Container>;
+};
+
+export default Desserts;
