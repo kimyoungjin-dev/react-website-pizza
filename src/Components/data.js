962d79557c8f901e49007840735ef12a9a61f02c
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-06 18:16:25 +0900

    Export productData and breadData to be used in the home screen as objects in the array.

---
+++ src/Components/data.js
@@ -0,0 +1,81 @@
+export const productData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1520201163981-8cc95007dd2a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8cGl6emF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    name: "Supreme Pizza",
+    desc:
+      "Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+    alt: "Pizza",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1584365685547-9a5fb6f3a70c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHBpenphfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    name: "Hawaiian Paradise",
+    desc:
+      "Marinara sauce, basil, italian sausage, romatomatoes, olives, and pesto",
+    price: "$16.99",
+    button: "Add to Cart",
+    alt: "Pizza",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1593246049226-ded77bf90326?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjl8fHBpenphfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    name: "Veggie Overload",
+    desc:
+      "Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "$14.99",
+    button: "Add to Cart",
+    alt: "Pizza",
+  },
+];
+
+export const FeatureData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1566843972142-a7fcb70de55a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzl8fHBpenphfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    title: "Pizza of the Day",
+    desc: "Truffle alfredo sauce topeed with 24 carat gold dust.",
+    button: "Order Now",
+  },
+];
+
+//bread
+
+export const breadData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1551024601-bec78aea704b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9udXR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    name: "Doughlicious",
+    desc:
+      "Belgian chocolate glaged donuts coveered in icing with sprinkles on top",
+    price: "$9.99",
+    button: "Add to Cart",
+    alt: "Bread",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1561845730-208ad5910553?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mjh8fGljZSUyMGNyZWFtfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    name: "Caramel Wonder",
+    desc:
+      "Vanilla ice cream covered with caramel and chocolate galze topped with a coco stick",
+    price: "$12.99",
+    button: "Add to Cart",
+    alt: "Bread",
+  },
+
+  {
+    image:
+      "https://images.unsplash.com/photo-1616631124190-fa90afc46fa7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fGJyb3duaWVpY2UlMjBjcmVhbXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    name: "Brownie Bunch",
+    desc:
+      "Double fudge brownie squares topped with white chocolate pieces and macadamia nuts",
+    price: "$9.99",
+    button: "Add to Cart",
+    alt: "Bread",
+  },
+];
